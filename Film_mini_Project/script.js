24aae7a4dc3389946c1f0e58e23c3a0abc43a55c
Vladislav <vlad0002411@gmail.com>
2020-11-18 02:32:08 +0300

    film second commit

--- Film_mini_Project/script.js
+++ Film_mini_Project/script.js
@@ -1,4 +1,7 @@
-const numberOfFilm = +prompt("Сколько фильмов вы уже посмотрели");
+"use strict";
+
+const numberOfFilm = +prompt("Сколько фильмов вы уже посмотрели"); // плюсик превращает строку в чисоловой тип
+// данных, если будет пустая строка в count оно закинет 0
 
 const personalMovieDB = {
     count: numberOfFilm,
@@ -8,14 +11,32 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Один из просмотренных фильмов",""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из просмотренных фильмов",""),
-      d = prompt("На сколько оцените его?", "");
+let lastFilm, num;
 
-personalMovieDB.movies[a] = b;  // когда записываем какие-то свойства в объект лучше использовать квадратные скобки,
-// это обезопасит от некоторых багов
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+        num = prompt("На сколько оцените его", "");
+
+    let temp = true;
+    while (temp) {
+        if (lastFilm == null || lastFilm == "" || lastFilm.length < 10 || num == null || num == "") {
+            lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+                num = prompt("На сколько оцените его?", "");
+        } else {
+            personalMovieDB.movies[lastFilm] = num;
+            temp = false;
+        }
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов"); // выскакивает обычное модальное окно с ОК
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
-
