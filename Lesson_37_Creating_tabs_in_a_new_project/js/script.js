3d6bbf59b42ae7ab3efa0bb73ba42895315256c0
Vladislav <vlad0002411@gmail.com>
2020-11-27 02:44:00 +0300

    Lesson 41, creating a countdown timer on the site

--- Lesson_37_Creating_tabs_in_a_new_project/js/script.js
+++ Lesson_37_Creating_tabs_in_a_new_project/js/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () =>{
+
+    // Tabs
+
     const tabs = document.querySelectorAll(".tabheader__item"),
           tabsContent = document.querySelectorAll(".tabcontent"),
           tabsParent = document.querySelector(".tabheader__items");
@@ -38,10 +41,60 @@ document.addEventListener("DOMContentLoaded", () =>{
     });
     
 
+    // Timer
 
+    const deadLine = "2020-11-30";
 
+    function GetTimeRemaining(endtime){
+        const temp = Date.parse(endtime) - Date.parse(new Date()), // парс мы используем потому что мы не смогли бы
+        // отнять дату в от того формата, которым мы задали в deadLine, в t лежит количество милисекунд
+        days = Math.floor(temp / (1000 * 60 * 60 * 24)),// 1000 это одна секунда * 60 вышло минута * 60 = час * 24=сутки
+        hours = Math.floor((temp / (1000 * 60 * 60) % 24)), // выйдет что мы количество дней делим на 24 часа и получаем
+        // оствток %, это и будет наши часы
+        minutes = Math.floor((temp / 1000 / 60)%60 ), // при первом делении получаем кол. сек. потом / 60 = кол. мин.
+        seconds = Math.floor((temp / 1000) % 60);
 
+        return { // возвращаем объект
+            "total": temp,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+    }  
 
+    function GetZero(num){ // исли у анс таймер будет с одной цифрой, будет добавлять спереди нолик
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = document.querySelector('#days'),
+              hours = document.querySelector('#hours'),
+              minutes = document.querySelector('#minutes'),
+              seconds = document.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000); // установили интервал запуска функции
+              updateClock();//чтобы когда обновляем не ставилась старая дата а потом через секунду уже менялась, а сразу
+
+        function updateClock(){
+            const temp = GetTimeRemaining(endtime);
+
+            days.innerHTML = GetZero(temp.days);
+            hours.innerHTML = GetZero(temp.hours);
+            minutes.innerHTML = GetZero(temp.minutes);
+            seconds.innerHTML = GetZero(temp.seconds);
+
+            if(temp.total <= 0){
+                clearInterval(timeInterval); // то будем останавливать на таймер
+            }
+        }
+    }
+
+    setClock(".timer", deadLine);
 
 
 
