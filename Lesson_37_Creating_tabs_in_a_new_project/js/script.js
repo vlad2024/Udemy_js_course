b55d13916358ec4909d607463b0e720d5fab9ae3
vlad <vlad0002411@gmail.com>
2021-01-08 20:13:50 +0300

    Lesson 48 Use classes

--- Lesson_37_Creating_tabs_in_a_new_project/js/script.js
+++ Lesson_37_Creating_tabs_in_a_new_project/js/script.js
@@ -132,7 +132,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     });
 
 
-    const modalTimerId = setTimeout(openModal, 3000); // устанавливаем таймер на открытие модального окна
+    //const modalTimerId = setTimeout(openModal, 3000); // устанавливаем таймер на открытие модального окна
 
     function ShowModalByScroll(){
         if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -143,4 +143,69 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
 
     window.addEventListener("scroll", ShowModalByScroll);   
+
+    class MenuCard{
+        constructor(src, alt, title, desc, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.desc = desc;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector); 
+            this.transfer = 27;
+            this.ChangeToUAN();
+        }
+
+        ChangeToUAN(){
+            this.price = this.price * this.transfer;
+        }
+
+        render(){
+            const element = document.createElement("div");
+            element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.desc}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+         </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    //const temp = new MenuCard(); // это все можно написать на много короче
+    //temp.render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        10,
+        ".menu .container"
+    ).render();
+
 });
