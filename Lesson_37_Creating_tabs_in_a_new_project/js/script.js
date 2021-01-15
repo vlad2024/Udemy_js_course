e8f95e7a7167a781cc96c0b1180f1bacd0fc5b57
vlad <vlad0002411@gmail.com>
2021-01-15 15:42:17 +0300

    Lesson 66 create a calculator on the site part1

--- Lesson_37_Creating_tabs_in_a_new_project/js/script.js
+++ Lesson_37_Creating_tabs_in_a_new_project/js/script.js
@@ -550,6 +550,82 @@ document.addEventListener("DOMContentLoaded", () =>{
     // });
 
 
+    // Calc
+
+    const result = document.querySelector(".calculating__result span");
+    let sex = "female", height, weight, age, ratio = 1.375;
+
+    function calcTotal(){
+        if(!sex || !height || !weight || !age || !ratio){
+            result.textContent = "_____";
+            return;
+        }
+
+        if(sex === "female"){ // для женщин
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } // Math.round округлили
+        else{ // для мужчин
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass){//передаем родителя, и изначальный эл активности
+        const elements = document.querySelectorAll(`${parentSelector} div`); // говорю что я буду получать 
+        // все div внутри родителя parentSelector
+
+        elements.forEach(elem =>{
+            elem.addEventListener("click", (e) =>{
+                if(e.target.getAttribute("data-ratio")){ // если у элемента по которому кликнули есть атрибут 
+                                                         // data-ratio то будем выполнять
+                    ratio = +e.target.getAttribute("data-ratio");
+                }
+                else{
+                    sex = e.target.getAttribute("id");
+                }
+    
+                elements.forEach(elem =>{ // убираю класс активности во всех элементах родителя
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass); // добавляю класс активности элементу на который нажали
+                calcTotal();
+            });
+            
+        });
+
+        
+    }
+
+    getStaticInformation("#gender", "calculating__choose-item_active");
+    getStaticInformation(".calculating__choose_big", "calculating__choose-item_active");
+
+    function getDynamicInformation(selector){
+        const input = document.querySelector(selector);
+
+        input.addEventListener("input", ()=>{
+            switch(input.getAttribute("id")){
+                case "height": 
+                    height = +input.value;
+                    break;
+                case "weight":
+                    weight = +input.value;
+                    break;
+                case "age":
+                    age = +input.value;
+                    break;
+
+            }
+
+            calcTotal();
+        });
+    }
+    
+    getDynamicInformation("#height");
+    getDynamicInformation("#weight");
+    getDynamicInformation("#age");
+
 });
 
 // API - интерфейс програмного обеспечения, или приложения(Простыми словами это набор данных и возможностей
